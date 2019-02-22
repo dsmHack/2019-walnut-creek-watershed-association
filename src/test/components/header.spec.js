48a8c4f0025ea7e98ea32cf0ee7cff8bbeee78ee
James <jamesalexchilders@gmail.com>
2019-02-22 03:14:50 -0600

    Fixing up testing for existing components

--- src/test/components/header.spec.js
+++ src/test/components/header.spec.js
@@ -4,10 +4,18 @@ import Header from '../../ui-core/components/header';
 
 describe("Header", () => {
     let wrapper;
+    let title;
+    
     beforeEach(() => {
-        wrapper = shallow(<Header title='test'/>);
-    })
+        title = chance.word();
+        wrapper = shallow(<Header title={title}/>);
+    });
+
     it('should render', () => {
         expect(wrapper.type()).toBe('div');
-    })
+    });
+
+    it('should set title', () => {
+        expect(wrapper.text()).toBe(title);
+    });
 })
