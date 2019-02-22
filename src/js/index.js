06571bb9ddac8fac6deb99fc1a12c851a7b0b5e6
Gujja, Reddy <NGujja@wrberkley.com>
2019-02-21 21:52:38 -0600

    reactjs index files

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    React.createElement("div", null, "Hello World"), // <div>Hello World</div>
+    document.getElementById("root")
+);
