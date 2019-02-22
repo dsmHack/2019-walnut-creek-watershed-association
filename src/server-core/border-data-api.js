93a9765b196f13a75fd12975c15075a3ef2e7aa9
John Fricano <FricanoJohnB@JohnDeere.com>
2019-02-22 16:26:07 -0600

    add api call for huc boundary data

---
+++ src/server-core/border-data-api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+async function getHucBorder(hucid, hucType) {
+
+    let url = "https://watersgeo.epa.gov/arcgis/rest/services/NHDPlus_NP21/WBD_NP21_Simplified/MapServer/find?searchText=" +
+        hucid + "&contains=true&searchFields=&sr=&layers=" +
+        hucType + "&layerDefs=&returnGeometry=true&maxAllowableOffset=&geometryPrecision=&dynamicLayers=&returnZ" +
+        "=false&returnM=false&gdbVersion=&returnUnformattedValues=false&returnFieldName=false&datumTransformations" +
+        "=&layerParameterValues=&mapRangeValues=&layerRangeValues=&f=pjson";
+
+    axios.get(url).then((response) => {
+        return response;
+    })
+        .catch((error) => {
+            console.log(error);
+            return "will the water kill me?";
+        });
+}
+
+module.exports = {getHucBorder};
