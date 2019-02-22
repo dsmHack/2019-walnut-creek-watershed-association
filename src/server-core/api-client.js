0af173d3104140d67a989fed98e5aa0fd5a57c01
Max Zimmerman <william.m.zimmerman@gmail.com>
2019-02-22 13:22:22 -0600

    Added api calls for ecoli and nitrate

--- src/server-core/api-client.js
+++ src/server-core/api-client.js
@@ -16,10 +16,26 @@ export default async function getEcoliData(huc) {
     });
 }
 
-async function getFibiData() {
+export async function getFibiData() {
     
 }
 
-async function getEcoliData() {
+export async function getNitrateData(huc) {
+    var url = "https://www.waterqualitydata.us/data/Result/search?"
+    var query = "startDateLo=01-01-2017&huc=" + huc + "&mimeType=xml&characteristicName=Nitrate";
+    ajax({
+        url: url + query,
+        type: "GET",
+        dataType: "xml",
+        success: function(data) {
+	    return data;
+        },
+        error: function(request, error) {
+            return "She get wet";
+        }
+    });
+}
+
+export async function getHuc(lat, long) {
     
 }
