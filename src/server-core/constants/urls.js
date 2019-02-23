b758de259011f70a6b686a4e5d24bd27fbe93582
Patrick Dean <patrick.l.dean@gmail.com>
2019-02-23 12:25:34 -0600

    clean up

--- src/server-core/constants/urls.js
+++ src/server-core/constants/urls.js
@@ -7,6 +7,8 @@ export const EPA_URL = "https://www.waterqualitydata.us/data/Station/search?";
 export const SAMPLE_RESULTS_URL =
     "https://www.waterqualitydata.us/data/Result/search?";
 export const ERROR_SHE_GET_WET = "She get wet";
+export const ERROR_ADDRESS_NOT_FOUND = "Address not found";
+export const ERROR_WATERSHED_NOT_FOUND = "Watershed not found";
 export const HUC_FROM_ADDRESS_URL =
     "https://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates";
 export const WATERSHED_DATA_URL =
