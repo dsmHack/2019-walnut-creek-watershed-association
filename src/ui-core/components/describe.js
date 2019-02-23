9ebde1e5e80d2f1277ba28b33f3d4d258fe38ef4
Gujja, Reddy <NGujja@wrberkley.com>
2019-02-22 20:20:32 -0600

    Add describe component

---
+++ src/ui-core/components/describe.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+class DescribeComponent extends React.Component {
+    constructor (props) {
+        super(props);
+    }
+
+    state = { showing: false };
+    render() {
+        const { showing } = this.state;
+        return (
+            <div>
+                <div onClick={() => this.setState({ showing: !showing })}>{this.props.shortText}</div>
+                { showing
+                    ? <div className="">
+                        {this.props.longText}
+                        </div>
+                    : null
+                }
+            </div>
+        );
+    }
+}
+
+export default DescribeComponent;
